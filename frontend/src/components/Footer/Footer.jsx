33286ee4994f1a0a82c5acb5742eb90ec59be79e
rabbyananto <rabby.ananto@gmail.com>
2024-04-30 00:14:31 +0200

    update : footer html and css

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum error
+            cum possimus magnam incidunt maxime repellendus provident
+            reprehenderit numquam culpa ab beatae dolorum voluptatem voluptatum
+            voluptate, illum quaerat ad, impedit amet quasi! Perspiciatis cum
+            minima dolorem consectetur, placeat in harum animi corporis,
+            laboriosam maxime mollitia obcaecati illo soluta est ex.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+49-1590-6414589</li>
+            <li>mdabdullaalrabby@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 © Md Abdulla Al Rabby - All rights reserved
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
