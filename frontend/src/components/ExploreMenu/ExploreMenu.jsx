0630ee42f5aca9033805e3b2864e258f38c4be48
rabbyananto <rabby.ananto@gmail.com>
2024-04-29 15:57:45 +0200

    frontend : header/hero section and menu section partial update with css

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./ExploreMenu.css";
+import { menu_list } from "../../assets/assets";
+
+const ExploreMenu = ({ category, setCategory }) => {
+  return (
+    <div className="explore-menu" id="explore-menu">
+      <h1>Explore our menu</h1>
+      <p className="explore-menu-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse placeat,
+        aliquam eligendi vel neque unde eos cumque accusantium delectus quam
+        vitae obcaecati, eius quasi sunt nostrum blanditiis repudiandae ad
+        animi.
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => {
+          return (
+            <div
+              onClick={() =>
+                setCategory((prev) =>
+                  prev === item.menu_name ? "All" : item.menu_name
+                )
+              }
+              key={index}
+              className="explore-menu-list-item"
+            >
+              <img
+                className={category === item.menu_name ? "active" : ""}
+                src={item.menu_image}
+                alt=""
+              />
+              <p>{item.menu_name} </p>
+            </div>
+          );
+        })}
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default ExploreMenu;
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your Favourite food here</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          voluptatibus cupiditate, vero delectus sequi nihil. Quasi cupiditate
+          tempore aut iusto nobis accusantium possimus vero, quidem saepe eos,
+          architecto cum asperiores.
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
